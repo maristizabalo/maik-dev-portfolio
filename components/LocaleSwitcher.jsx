1bcd37e5c8a591a719159a5c9c8d52eef26996ab
Maicol Aristizabal <maristizabalo95@gmail.com>
2024-06-20 23:18:35 -0500

    Arreglando LocalSwitcher

--- components/LocaleSwitcher.jsx
+++ components/LocaleSwitcher.jsx
@@ -1,36 +1,9 @@
-"use client";
-
-import { useState, useEffect, useTransition } from 'react';
-// import { CiLight, CiDark } from "react-icons/ci";
-import { Switch } from '@nextui-org/react';
-import { useLocale } from 'next-intl';
 
 const LocaleSwitcher = () => {
-    const locale = useLocale()
-    const [startTransition] = useTransition();
-    const handleLanguage = () => {
-        if (locale === "en") {
-            startTransition(() => {
-                
-            })
-        } else if(locale === "es"){
-            startTransition(() => {
-                
-            })
-        }
-    }
 
     return (
         <div>
-            <Switch
-                defaultSelected
-                size='lg'
-                color='primary'
-                onClick={handleLanguage}
-                startContent={<CiLight />}
-                endContent={<CiDark />}
-            >
-            </Switch>
+            HOLA
         </div>
     )
 }
--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { CiLight, CiDark } from "react-icons/ci";
-import { Switch } from '@nextui-org/react';
 
 const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false)
