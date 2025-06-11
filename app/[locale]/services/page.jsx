b46c39724d9131413fd467c76dd79c52e996008b
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2025-06-11 10:57:03 -0500

    Modificando algunos colores

--- app/[locale]/services/page.jsx
+++ app/[locale]/services/page.jsx
@@ -24,7 +24,7 @@ const Services = () => {
               <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
                 {/* top */}
                 <div className='w-full flex justify-between items-center'>
-                  <div className='text-5xl font-extrabold text-outline dark:text-transparent text-black group-hover:text-outline-hover transition-all duration-500'>{t(`num0${index}`)}</div>
+                  <div className='text-5xl font-extrabold text-outline dark:text-transparent group-hover:text-outline-hover transition-all duration-500'>{t(`num0${index}`)}</div>
                   <Link href={t(`href0${index}`)}
                     className='w-[70px] h-[70px] rounded-full dark:bg-white bg-gray-500 group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'
                   >
