fbc292c94d72e1ac3b5db05f160841de910fd144
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2025-06-14 18:57:47 -0500

    Colores texto en page/contact

--- app/[locale]/contact/page.jsx
+++ app/[locale]/contact/page.jsx
@@ -24,7 +24,7 @@ const info = [
   {
     icon: <FaEnvelope />,
     title: "Email",
-    description: "myemail@gmail.com"
+    description: "maristizabalo95@gmail.com"
   },
   {
     icon: <FaMapMarkedAlt />,
@@ -54,11 +54,11 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px] ">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form className="flex flex-col gap-6 p-10 bg-primary dark:bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">
                 Lets work together
               </h3>
-              <p className="text-white/60">
+              <p className="text-white dark:text-white/60">
                 Lorem ipsum doler cosectelur adicpidn elit. Eim minhil sapiente suma pariatur ed total.
               </p>
               {/* input */}
@@ -104,11 +104,11 @@ const Contact = () => {
               {info.map((item, index) => {
                 return (
                   <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-primary dark:bg-[#27272c] text-accent rounded-md flex items-center justify-center">
                       <div className="text-[28px]">{item.icon}</div>
                     </div>
                     <div className="flex-1">
-                      <p className="text-white/60">{item.title}</p>
+                      <p className="text-gray-800 dark:text-white/60">{item.title}</p>
                       <h3 className="text-xl">{item.description}</h3>
                     </div>
                   </li>
